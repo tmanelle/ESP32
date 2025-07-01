32186e69b2835d4db3592d4022e0212b839ba102
tmanelle <manelle.tahiri@ynov.com>
2025-07-01 14:37:28 +0200

    Ajout LED verte (GPIO19)

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 
-#define LED_RED 12 // GPIO12 = CMD_LED_RED
+#define LED_RED 12   // GPIO12 = CMD_LED_RED
+#define LED_VERTE 19 // GPIO19 = CMD_LED_VERTE
 
 void setup()
 {
@@ -9,8 +10,15 @@ void setup()
 
 void loop()
 {
+  // Clignote LED rouge
   digitalWrite(LED_RED, HIGH); // Allume la LED
   delay(500);                  // Attendre 500 ms
   digitalWrite(LED_RED, LOW);  // Éteint la LED
   delay(500);                  // Attendre 500 ms
+
+  // Clignote LED verte
+  digitalWrite(LED_VERTE, HIGH);
+  delay(500);
+  digitalWrite(LED_VERTE, LOW);
+  delay(500);
 }
