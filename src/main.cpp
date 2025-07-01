d7ffeb19138379a0539076498634f393482e0bfe
tmanelle <manelle.tahiri@ynov.com>
2025-07-01 14:32:00 +0200

    ✨ Ajout du clignotement de la LED rouge (GPIO12)

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,16 @@
 #include <Arduino.h>
 
-// Choisir la bonne broche pour la LED
-#define LED_PIN 2 // essaie GPIO2, souvent utilisée par défaut
+#define LED_RED 12 // GPIO12 = CMD_LED_RED
 
 void setup()
 {
-  pinMode(LED_PIN, OUTPUT); // Configure la pin en sortie
+  pinMode(LED_RED, OUTPUT);
 }
 
 void loop()
 {
-  digitalWrite(LED_PIN, HIGH); // Allume la LED
-  delay(500);                  // Pause de 500 ms
-  digitalWrite(LED_PIN, LOW);  // Éteint la LED
-  delay(500);                  // Pause de 500 ms
+  digitalWrite(LED_RED, HIGH); // Allume la LED
+  delay(500);                  // Attendre 500 ms
+  digitalWrite(LED_RED, LOW);  // Éteint la LED
+  delay(500);                  // Attendre 500 ms
 }
